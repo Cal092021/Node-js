0a523cdcecd86e986c0f265059a6f2c27f54e7fa
Alex Calhoun <alxcalhoun@gmail.com>
2022-01-31 12:09:59 +0000

    updated

---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+console.log(process.argv);
+
+const movieArr = [];
+
+
+class Movie {
+    constructor(title, actor = "Not specified") {
+    this.title = title;
+    this.actor = actor;
+}
+
+    add() {
+        movieArr.push(this);
+    }
+
+}
+
+module.exports = {Movie, movieArr};
